8c7178f0b3633edd56faee4582dde453809a09f9
lpcooper-arch <lpcooper@bu.edu>
2025-09-16 18:02:46 -0400

    Update Assign02_03.java

--- assigns/02/MySolution/Assign02_03.java
+++ assigns/02/MySolution/Assign02_03.java
@@ -1,8 +1,18 @@
 public class Assign02_03 {
     public static boolean solve_3sum(Integer[] A) {
-	
+    	for (int i = 0; i < A.length; i++) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (Assign02_02.indexOf(A, A[i] + A[j]) != -1) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
-	// Please write some code here for testing solve_3sum
+	    final Integer[] arr = {1, 3, 5, 7, 9};
+        final Integer[] arr2 = {1, 3, 5, 4, 10};
+        System.out.println(solve_3sum(arr));    // Output: false
+        System.out.println(solve_3sum(arr2));   // Output: true
     }
 }
